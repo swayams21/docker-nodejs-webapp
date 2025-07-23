75707d2823f4fd740520a77786fc6ccbcfff769a
swayamsiddha savekar <swayamsavekar21@gmail.com>
2025-07-23 22:10:40 +0530

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,1 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello from Dockerized Node.js App 🚀');
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
 
